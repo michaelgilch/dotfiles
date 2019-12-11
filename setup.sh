eaf036c8ff4d2e3768bbdfacf43a6496b8c30b4f
Michael Gilchrist <michaelgilch@gmail.com>
2019-12-11 13:23:51 -0500

    Add setup file for dotfiles installation

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# setup.sh
+# Creates symlinks from dotfiles dir to proper config locations.
+# 
+# Michael Gilchrist <michaelgilch@gmail.com>
+
+DOTFILES_DIR=$(pwd)
+CONFIG_DIR="$HOME"/.config
+
+create_symlink()
+{
+    [[ -e $2 ]] && rm "$2"
+    ln -s "$1" "$2"
+}
+
+# create configuration directory if it does not yet exist
+[[ -d "$CONFIG_DIR" ]] || mkdir "$CONFIG_DIR"
+
+# bash
+[[ $(pacman -Q bash 2>/dev/null) != "" ]] && {
+    create_symlink "$DOTFILES_DIR"/bash/bashrc          "$HOME"/.bashrc
+    create_symlink "$DOTFILES_DIR"/bash/bash_profile    "$HOME"/.bash_profile
+    create_symlink "$DOTFILES_DIR"/bash/bash_logout     "$HOME"/.bash_logout
+    create_symlink "$DOTFILES_DIR"/bash/bash_aliases    "$HOME"/.bash_aliases
+    create_symlink "$DOTFILES_DIR"/bash/bash_functions  "$HOME"/.bash_functions
+    create_symlink "$DOTFILES_DIR"/bash/bash_prompts    "$HOME"/.bash_prompts
+    create_symlink "$DOTFILES_DIR"/bash/less_termcap    "$HOME"/.less_termcap
+}
+
+# git 
+[[ $(pacman -Q git 2>/dev/null) != "" ]] && \
+    create_symlink "$DOTFILES_DIR"/git/gitconfig        "$HOME"/.gitconfig
+
+# vim
+[[ $(pacman -Q vim 2>/dev/null) != "" ]] && \
+    create_symlink "$DOTFILES_DIR"/vim                  "$HOME"/.vim
